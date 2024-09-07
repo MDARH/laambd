3d0be81a8c5d2a9b8b69e39d1c6f2d6d81d821c1
Hassan Laptop-7400 <mdarh411@gmail.com>
2024-09-07 13:56:07 +0600

    fix the space and yt iframe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Styles -->
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 </head>
 <body>
     <div class="bg-[#064403] flex flex-col justify-center items-center">
@@ -38,42 +39,46 @@
             </button>
         </div>
     </div>
-    <div class="bg-[#028833] flex flex-col justify-center items-center">
+    <div class="bg-[#028833]">
+        <div class="w-full md:w-8/12 mx-auto p-4">
 
-        <div class="py-4 mx-2 text-[#ffec00] font-bold border-2 rounded-3xl bg-[#008000] p-2">
-            কালোজিরা রসুন মধু এবং ইরানী জাফরান মসলা মিশ্রণটি আপনি কেন খাবেন?
-        </div>
-        <div class="border-4 border-[#ffec00] shadow-3xl rounded-3xl mx-2 my-4">
-            <ul class="p-2">
-                <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted">
-                    <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য জীবন
-                </li>
-                <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted">
-                    <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য জীবন
-                </li>
-                <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted">
-                    <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য জীবন
-                </li>
-                <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted">
-                    <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য
-                </li>
-                <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted">
-                    <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য জীবন
-                </li>
-            </ul>
-        </div>
-        <div class="px-2 pb-4">
-            <div class="flex justify-center items-center gap-2 font-hind text-3xl text-[#064403] font-bold bg-white border-2 border-[#ffec00] rounded py-2">
-                <span>কল করুন</span>
-                <x-fas-phone-alt class="h-6" />
-                <span>+880 1606-700289</span>
-            </div>`
+            <h4 class="bg-[#008000] text-[#ffec00] text-center font-bold text-2xl p-4 border-2 rounded-3xl">
+                কালোজিরা রসুন মধু এবং ইরানী জাফরান মসলা মিশ্রণটি আপনি কেন খাবেন?
+            </h4>
+            <div class="border-4 border-[#ffec00] shadow-3xl rounded-3xl p-4 my-4">
+                <ul class="p-2">
+                    <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted my-4 py-2">
+                        <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য জীবন
+                    </li>
+                    <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted my-4 py-2">
+                        <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য জীবন
+                    </li>
+                    <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted my-4 py-2">
+                        <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য জীবন
+                    </li>
+                    <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted my-4 py-2">
+                        <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য
+                    </li>
+                    <li class="flex gap-2 items-center text-white text-lg border-b-2 border-dotted my-4 py-2">
+                        <x-fas-check-circle class="text-[#ffec00] h-8" /> শারীরিক দুর্বলতা বা যৌন দুর্বলতা দূর করে আপনাকে উপহার দিবে সুখময় দাম্পত্য জীবন
+                    </li>
+                </ul>
+            </div>
+
+            <div class="px-2">
+                <div class="flex justify-center items-center gap-2 font-hind text-3xl text-[#064403] font-bold bg-white border-2 border-[#ffec00] rounded py-2">
+                    <span>কল করুন</span>
+                    <x-fas-phone-alt class="h-6" />
+                    <span>+880 1606-700289</span>
+                </div>`
+            </div>
         </div>
     </div>
     <section class="bg-[#064403]">
-        <div class="mx-2 py-10">
-            <div class="flex">
-                <div class="w-1/2">
+        <div class="w-full md:w-10/12 mx-auto p-4">
+
+            <div class="flex items-center">
+                <div class="w-1/2 py-10">
                     <h2 class="text-[#ffec00] bg-[#008000] font-bold text-center text-2xl drop-shadow-2xl p-4 border-2 rounded-xl"> উপরের সমস্যা গুলোর সমাধান না করলে যে ভয়াবহ সমস্যা গুলো হবে</h2>
 
                     <div class="border-4 border-[#ffec00] shadow-3xl rounded-3xl mx-2 my-4">
@@ -95,11 +100,18 @@
                             </li>
                         </ul>
                     </div>
-                    <div>
-                        <h3>খাওয়ার নিয়মাবলীঃ</h3>
-                        <ul>
-                            <li>সকালে নাস্তা খাওয়া পর এক চামচ।</li>
-                            <li>রাতে খাবারের পরে দুই চামচ।</li>
+                    <div class="my-10">
+                        <h3 class="text-[#ffec00] text-4xl font-bold font-hind">খাওয়ার নিয়মাবলীঃ</h3>
+
+                        <ul class="my-4 text-2xl text-white font-semibold font-hind">
+                            <li class="flex gap-2">
+                                <x-fas-check-circle class="h-6 text-white" />
+                                <span class="text-[#ffec00]">সকালে নাস্তা খাওয়া পর</span><span>এক চামচ।</span>
+                            </li>
+                            <li class="flex gap-2">
+                                <x-fas-check-circle class="h-6 text-white" />
+                                <span>রাতে খাবারের পরে</span>
+                                <span class="text-[#ffec00]"> দুই চামচ।</span></li>
                         </ul>
                         <div class="py-4 ">
                             <button class="flex items-center gap-2 text-white bg-[#05B858] text-xl font-bold px-4 py-2 border-white border-2 font-hind rounded-full">
@@ -109,82 +121,169 @@
                     </div>
                 </div>
                 <div class="w-1/2 mx-auto">
-                    <img class="mx-auto w-3/4 rounded border-2 border-[#ffec00]" src="https://i0.wp.com/laambd.com/wp-content/uploads/2024/01/image_ajS.webp?w=1024&ssl=1" alt="" class="w-96">
+                    <img class="mx-auto w-5/6 rounded border-2 border-[#ffec00]" src="https://i0.wp.com/laambd.com/wp-content/uploads/2024/01/image_ajS.webp?w=1024&ssl=1" alt="" class="w-96">
                 </div>
             </div>
         </div>
     </section>
-    <section class="bg-[#028833]">
-        <div>
-            <h2>এটি সম্পূর্ণ হোমমেড একটি প্রোডাক্ট তাই কোন পার্শ্ব প্রতিক্রিয়া নেই। <span>লিমিটেড স্টকলিমিটেড স্টক,</span> তাই দেরি না করে এখন-ই <span>অর্ডার করুন।</span></h2>
-            <div class="px-2 pb-4">
-                <div class="flex justify-center items-center gap-2 font-hind text-3xl text-[#064403] font-bold bg-white border-2 border-[#ffec00] rounded py-2">
+    <section class="bg-[#028833] py-10">
+        <div class="w-8/12 mx-auto">
+            <h2 class="text-white text-3xl font-semibold text-center">
+                <span>এটি সম্পূর্ণ হোমমেড একটি প্রোডাক্ট তাই কোন পার্শ্ব প্রতিক্রিয়া নেই।</span>
+                <span class="text-[#ffec00]">লিমিটেড স্টকলিমিটেড স্টক,</span>
+                <span>তাই দেরি না করে এখন-ই</span>
+                <span class="text-[#ffec00]">অর্ডার করুন।</span>
+            </h2>
+            <div class="p-4">
+                <div class="flex justify-center items-center gap-2 font-hind text-3xl text-[#ffffff] font-bold border-2 border-[#ffec00] rounded py-2">
                     <span>কল করুন</span>
                     <x-fas-phone-alt class="h-6" />
                     <span>+880 1606-700289</span>
                 </div>`
             </div>
-            <div>
-                <p>⭐⭐⭐ সম্মানিত কাস্টমারদের মতামত ⭐⭐⭐</p>
+            <div class="text-center text-4xl font-bold text-white py-8">
+                ⭐⭐⭐
+                <span>সম্মানিত</span>
+                <span class="text-[#ffec00]">কাস্টমারদের মতামত</span>
+                ⭐⭐⭐
             </div>
-            <div>
-                <div>
-                    <p>১ কেজি কালোজিরা রসুন মধু এবং ইরানী জাফরান মসলা মিশ্রণ এর রেগুলার প্রাইস ২০০০ টাকা</p>
-                    <p>তবে বর্তমানে অফার প্রাইস ১৫৮০ টাকা</p>
-                </div>
-                <div>
-                    <p>১ কেজি কালোজিরা রসুন মধু এবং ইরানী জাফরান মসলা মিশ্রণ এর রেগুলার প্রাইস ২০০০ টাকা</p>
-                    <p>তবে বর্তমানে অফার প্রাইস ১৫৮০ টাকা</p>
-                </div>
+        </div>
+        <div class="w-10/12 flex gap-2">
+            <div class="border-2 border-[#ffec00] w-96">
+                <iframe width="380" height="550" src="https://www.youtube.com/embed/QbPghkSWvCc?si=QvRNMKhvJ9J9Bp_O" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
-            <div>
-                <div>
-                    <p>
-                        অবশ্যই ৪৫/৬০ দিন খেতে হবে সেক্ষেত্রে ১ কেজির ফুল কোর্স অর্ডার করা উত্তম হবে এবং ফুল কোর্স শেষ করতে পারলে যে রোগের জন্য নিচ্ছেন সেই রোগ টি ভিতর থেকে সারাজিবনের জন্য নিঃশেষ হয়ে যাবে ইনশাআল্লাহ
-                    </p>
+
+            <div class="swiper h-9 w-96">
+                <!-- Additional required wrapper -->
+                <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    <div class="swiper-slide">Slide 1</div>
+                    <div class="swiper-slide">Slide 2</div>
+                    <div class="swiper-slide">Slide 3</div>
+                    ...
                 </div>
-                <div>
-                    <p>আজকে অর্ডার করলে হোম ডেলিভারি চার্জ ফ্রি।</p>
-                </div>
+                <!-- If we need pagination -->
+                <div class="swiper-pagination"></div>
+
+                <!-- If we need navigation buttons -->
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+
+                <!-- If we need scrollbar -->
+                <div class="swiper-scrollbar"></div>
             </div>
+
+
+            {{-- <div class="relative w-full max-w-lg mx-auto">
+                <!-- Slideshow Images -->
+                <div class="slideshow-container">
+                    <div class="mySlides">
+                        <img class="h-[540px] "
+                        src="https://i0.wp.com/laambd.com/wp-content/uploads/2024/07/450723203_122156567780152330_406095415020683934_n.jpg?fit=526%2C1169&ssl=1" alt="">
+                    </div>
+                    <div class="mySlides">
+                        <img class="h-[540px] " src="https://i0.wp.com/laambd.com/wp-content/uploads/2024/07/450723203_122156567780152330_406095415020683934_n.jpg?fit=526%2C1169&ssl=1" alt="">
+                    </div>
+                    <div class="mySlides">
+                        <img class="h-[540px] " src="https://i0.wp.com/laambd.com/wp-content/uploads/2024/07/450723203_122156567780152330_406095415020683934_n.jpg?fit=526%2C1169&ssl=1" alt="">
+                    </div>
+                </div>
+
+                <!-- Dot Navigation -->
+                <div class="flex justify-center mt-4">
+                    <span class="dot h-4 w-4 mx-1 rounded-full bg-gray-400 cursor-pointer"></span>
+                    <span class="dot h-4 w-4 mx-1 rounded-full bg-gray-400 cursor-pointer"></span>
+                    <span class="dot h-4 w-4 mx-1 rounded-full bg-gray-400 cursor-pointer"></span>
+                </div>
+            </div> --}}
+
         </div>
     </section>
     <section>
-        <div class="border text-4xl shadow ">
-            আজকে অর্ডার করলে হোম ডেলিভারি চার্জ ফ্রি।
+        <div>
+            <div>
+                <p>১ কেজি কালোজিরা রসুন মধু এবং ইরানী জাফরান মসলা মিশ্রণ এর রেগুলার প্রাইস ২০০০ টাকা</p>
+                <p>তবে বর্তমানে অফার প্রাইস ১৫৮০ টাকা</p>
+            </div>
+            <div>
+                <p>১ কেজি কালোজিরা রসুন মধু এবং ইরানী জাফরান মসলা মিশ্রণ এর রেগুলার প্রাইস ২০০০ টাকা</p>
+                <p>তবে বর্তমানে অফার প্রাইস ১৫৮০ টাকা</p>
+            </div>
         </div>
-        <form action="" class="border-2 shadow-md border-[#064403]">
+        <div>
+            <div>
+                <p>
+                    অবশ্যই ৪৫/৬০ দিন খেতে হবে সেক্ষেত্রে ১ কেজির ফুল কোর্স অর্ডার করা উত্তম হবে এবং ফুল কোর্স শেষ করতে পারলে যে রোগের জন্য নিচ্ছেন সেই রোগ টি ভিতর থেকে সারাজিবনের জন্য নিঃশেষ হয়ে যাবে ইনশাআল্লাহ
+                </p>
+            </div>
+            <div>
+                <p>আজকে অর্ডার করলে হোম ডেলিভারি চার্জ ফ্রি।</p>
+            </div>
+        </div>
+    </section>
+    <section class="py-4">
+        <div class="border rounded-lg text-4xl shadow-laam mx-auto w-8/12 my-4 p-4">
+            <h6 class="text-center text-[#056C00] font-bold">
+                অর্ডার করতে আপনার সঠিক তথ্য দিয়ে নিচের ফর্মটি সম্পূর্ণ পূরন করুন
+            </h6>
+        </div>
+        <form action="" class="border-4 rounded-lg shadow-form border-[#098D2E] w-8/12 mx-auto">
+            <div class="p-8">
+                <div class="text-xl font-semibold py-8">
+                    পরিমান সিলেক্ট করুন
+                </div>
+                <div class="grid grid-cols-2 gap-2">
+                    <button class="border rounded p-6">
+                        <div class="flex gap-2">
+                            <input type="radio">
+                            <img class="h-12" src="https://i0.wp.com/laambd.com/wp-content/uploads/2024/01/018-4.png?resize=300%2C300&ssl=1" alt="Product">
+                            <h4 class="text-left font-semibold">কালোজিরা রসুন মধু এবং ইরানি জাফরান মসলা ৫০০ গ্রাম</h4>
+                        </div>
+                        <div class="float-end">
+                            <input type="number">
+                            <p>2,940.00৳</p>
+                        </div>
+                    </button>
+                    <button class="border rounded p-6">
+                        <div class="flex gap-2">
+                            <input type="radio">
+                            <img class="h-12" src="https://i0.wp.com/laambd.com/wp-content/uploads/2024/01/018-4.png?resize=300%2C300&ssl=1" alt="Product">
+                            <h4 class="text-left font-semibold">কালোজিরা রসুন মধু এবং ইরানি জাফরান মসলা ৫০০ গ্রাম</h4>
+                        </div>
+                        <div class="float-end">
+                            <input type=" ">
+                            <p>2,940.00৳</p>
+                        </div>
+                    </button>
+                </div>
+                <div class="">
+                    <div>
+                        বিলিং এড্রেস
+                        <div>
+                            <label for="name">আপনার নাম লিখুন</label>
+                            <input type="text" id="name" placeholder="নাম লিখুন">
 
-            <div class="border rounded-md">
-                পরিমান সিলেক্ট করুন
-            </div>
-            <div>
-                <div>product 1</div>
-                <div>product 2</div>
-            </div>
-            <div class="flex justify-center">
-                <div>
-                    Billing Address
-                    <div>
-                        <label for="your_name">আপনার নাম লিখুন</label>
-                        <input type="text" placeholder="নাম লিখুন">
-                        <p>error</p>
+                            <p>error</p>
+                        </div>
+                        <div>
+                            <label for="address">আপনার সম্পূর্ণ ঠিকানা লিখুন</label>
+                            <input type="text" id="address" placeholder="ঠিকানা লিখুন, থানা ও জেলা সহ">
+
+                        </div>
+                        <div>
+                            <label for="mobile_number">আপনার মোবাইল নাম্বার লিখুন</label>
+                            <input type="text" id="mobile_number" placeholder="মোবাইল নাম্বার">
+
+                        </div>
                     </div>
                     <div>
-                        <label for="address">আপনার সম্পূর্ণ ঠিকানা লিখুন</label>
-                        <input type="text" placeholder="ঠিকানা লিখুন, থানা ও জেলা সহ">
-                    </div>
-                    <div>
-                        <label for="mobile_number">আপনার মোবাইল নাম্বার লিখুন</label>
-                        <input type="text" placeholder="মোবাইল নাম্বার">
+                        Apnar Order
                     </div>
                 </div>
                 <div>
-                    Apnar Order
+                    এখানে ক্লিক করে অর্ডারটি সম্পন্ন করুন
                 </div>
-            </div>
-            <div>
-                Button
+
             </div>
         </form>
     </section>
@@ -193,10 +292,35 @@
             <li>Privacy Policy</li>
             <li>Terms & conditions</li>
         </ul>
-        <div>  
+        <div>
             Copyright © 2023 Laam BD | All Rights Reserved
         </div>
     </footer>
 
+    <script>
+        const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'vertical'
+            , loop: true,
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination'
+            , },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next'
+                , prevEl: '.swiper-button-prev'
+            , },
+
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar'
+            , }
+        , });
+
+    </script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ export default {
         tiro_bn: ['Tiro Bangla', 'sans-serif'],
       },
       boxShadow: {
-        '3xl': '0px 0px 10px 0px rgba(255, 241, 67.99999999999999, 0.8) inset'
+        '3xl': '0px 0px 10px 0px rgba(255, 241, 67.99999999999999, 0.8) inset',
+        'form': '0px 0px 10px 0px rgba(5.9999999999999725, 68.00000000000001, 2.9999999999999973, 0.8) inset',
+        'laam': '0px 0px 10px 0px rgba(0,0,0,0.5) inset'
       }
     },
   },
